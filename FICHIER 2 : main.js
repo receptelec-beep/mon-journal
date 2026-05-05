8142332b91d39968e6b31d7a7655cb6e8c6fd950
receptelec-beep <receptelec@icloud.com>
2026-05-05 19:11:20 +0200

    Update and rename FICHIER 2 : main.jsx to FICHIER 2 : main.js

--- FICHIER 2 : main.js
+++ FICHIER 2 : main.js
@@ -2,6 +2,6 @@ import React from "https://esm.sh/react";
 
 import { createRoot } from "https://esm.sh/react-dom/client";
 
-import App from "./App.jsx";
+import App from "./App.js";
 
 createRoot(document.getElementById("root")).render(<App />);
