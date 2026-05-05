df8b40e60318e3f2b1fa6e8751558e8f18c8cafd
receptelec-beep <receptelec@icloud.com>
2026-05-05 18:21:16 +0200

    Create FICHIER 2 : main.jsx

---
+++ FICHIER 2 : main.jsx
@@ -0,0 +1,7 @@
+import React from "https://esm.sh/react";
+
+import { createRoot } from "https://esm.sh/react-dom/client";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
