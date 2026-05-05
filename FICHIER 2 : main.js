ee1046a4c5d6e34db833291df551167d879aca45
receptelec-beep <receptelec@icloud.com>
2026-05-05 19:25:31 +0200

    Update FICHIER 2 : main.js

--- FICHIER 2 : main.js
+++ FICHIER 2 : main.js
@@ -4,4 +4,8 @@ import { createRoot } from "https://esm.sh/react-dom/client";
 
 import App from "./App.js";
 
+import { createRoot } from "https://esm.sh/react-dom/client";
+
+import App from "./App.js";
+
 createRoot(document.getElementById("root")).render(<App />);
